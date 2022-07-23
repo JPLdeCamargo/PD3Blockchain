377d4e34a908daa12cedf5b07215df22ec9c30fa
Enzo <enzobassani09@gmail.com>
2022-07-23 19:13:50 -0300

    update BlockchainAPI

--- App/BlockchainAPI.py
+++ App/BlockchainAPI.py
@@ -4,23 +4,23 @@ class BlockchainAPI:
     def __init__(self):
         pass
 
-    def addPost(post_text: str, post_media_link: str, date: datetime, private_key: int):
+    def addPost(post_text: str, post_media_link: str, date: datetime, address: int, private_key: int):
         pass
 
-    def addBet(postID: int, betted_amount: int, private_key: int):
+    def addBet(postID: int, betted_amount: int, address: int, private_key: int):
         pass
 
-    def addValidityMedia(postID: int, text: str, link: str, private_key: int):
+    def addValidityMedia(postID: int, text: str, link: str, address: int, private_key: int):
         pass
 
-    def vote(postID: int, is_valid: bool, private_key: int):
+    def vote(postID: int, is_valid: bool, address: int, private_key: int):
         pass
 
-    def enableVotes(postID: int, private_key: int):
+    def enableVotes(postID: int, address: int, private_key: int):
         pass
 
-    def terminatePost(postID: int, private_key: int):
+    def terminatePost(postID: int, address: int, private_key: int):
         pass
 
-    def getPost(postID: int, private_key: int):
+    def getPost(postID: int, address: int, private_key: int):
         pass
